82097e3a8cd60041802f4db74ffa96b11f7a909a
Max <maximilian.kaplan@gmail.com>
2021-08-16 20:52:14 -0700

    init

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ URL_BASE = "https://app.pavlok.com/unlocked/remotes/{}/zap/{}?message={}"
 
 def make_url(name, level, message=""):
     return URL_BASE.format(name, level, message)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
